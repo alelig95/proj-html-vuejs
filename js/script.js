6f97d9c9976c4f859aa34bd4b42b4920031121c9
Alessandro Liggieri <smmb@live.it>
2021-02-23 16:20:52 +0100

    Lavorando sugli hover

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 new Vue ({
   el: '#app',
   data: {
-    navbarLinks: ['Home', 'About', 'Services', 'Blog', 'Contact', 'Portfolio', 'Sign In']
+    logo: 'images/white-logo-2.png',
+    navbarLinks: ['Home', 'About', 'Services', 'Blog', 'Contact', 'Portfolio'],
+    footerQuickLinks: ['Services', 'Contact', 'Blog'],
+    footerResources: ['Art Design', 'Computer', 'IT News', 'Networking', 'Web Security'],
+    footerContactUs: [
+      {
+          address: 'Patricia C. Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051',
+          phone: '+99 (0) 101 0000 888',
+          email: 'Info@yourdomain.com',
+      }
+    ]
+    
   }
 });
 Vue.config.devtools = true;
